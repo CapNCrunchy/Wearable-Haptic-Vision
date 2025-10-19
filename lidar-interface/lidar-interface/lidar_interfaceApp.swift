33a055d1893726ab6369eabf3d7c7a78e57b7eb7
Sniped <me@colin.gg>
2025-10-19 11:52:26 -0400

    (lidar) init swift project

---
+++ lidar-interface/lidar-interface/lidar_interfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  lidar_interfaceApp.swift
+//  lidar-interface
+//
+//  Created by Colin McClure on 10/19/25.
+//
+
+import SwiftUI
+
+@main
+struct lidar_interfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
