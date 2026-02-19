95913bd3a3cd8c60f351b7ff15455d3251cd05a2
Sniped <me@colin.gg>
2026-02-19 15:02:57 -0500

    session view

---
+++ lidar-interface/lidar-interface/Presentation/SessionView.swift
@@ -0,0 +1,14 @@
+//
+//  SessionView.swift
+//  lidar-interface
+//
+//  Created by Colin McClure on 2/19/26.
+//
+
+import SwiftUI
+
+struct SessionView: View {
+    var body: some View {
+        
+    }
+}
